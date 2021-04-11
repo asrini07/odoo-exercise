7fa9d988eb790f2b00802dd6ab66077fae11afae
Aas Asrini <asrinirpl07@gmail.com>
2021-04-11 16:55:36 +0700

    update

---
+++ openacademy/models/models.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class openacademy(models.Model):
+#     _name = 'openacademy.openacademy'
+#     _description = 'openacademy.openacademy'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+
+# from odoo import models, fields, api, exeption
+
+# class Course(models.Model):
+#     _name = 'openacademy.course'
+#     _description = "OpenAcademy Courses"
+
+#     name = fields.Char(string="Title", required=True)
+#     description = fields.Text()
+
+from odoo import models, fields, api
+
+class Course(models.Model):
+    _name = 'openacademy.course'
+    _description = "OpenAcademy Courses"
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
